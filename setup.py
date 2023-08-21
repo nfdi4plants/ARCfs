29be8d9fadff342bf96abdd29b2d9be7cfc95b5e
Julian <weidhase.jw@gmail.com>
2023-08-21 14:37:25 +0200

    Updating license information.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
             'arcfs = gitlab_arc_fs.opener:ARCfsOpener',
         ]
     },
-    license="MIT License",
+    license="GPLv3",
     packages=['gitlab_arc_fs'],
     package_dir={'gitlab_arc_fs': 'src/gitlab_arc_fs'},
     version="0.0.15.dev1",
